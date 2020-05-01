954c25851b2024b58788cb25296c45c736979743
ksana404 <ksana404@163.com>
2020-05-01 13:05:09 +0800

    Git-dev程 分支测试

---
+++ yangyang/src/test/java/com/yg/pj/sys/source/GitTest.java
@@ -0,0 +1,13 @@
+package com.yg.pj.sys.source;
+
+public class GitTest {
+	
+	public static void main(String[] args) {
+		//Git 上传到远程 分支测试
+		String str = "";
+		String str01 = "";
+		String str02 = "";
+		String str03 = "";
+	}
+
+}
