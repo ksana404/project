31d54c9c0c2e0e1dbc8c1e98198e999d7b645143
ksana404 <ksana404@163.com>
2020-05-01 18:03:05 +0800

    这是一个dev00,第一个分支

--- yangyang/src/test/java/com/yg/pj/sys/git/GitBranch.java
+++ yangyang/src/test/java/com/yg/pj/sys/git/GitBranch.java
@@ -2,7 +2,8 @@ package com.yg.pj.sys.git;
 
 public class GitBranch {
 	public static void main(String[] args) {
-		System.out.println("创建分支第一步 ");
+		System.out.println("这是第一个 dev ");
+		System.out.println("大家好,我是 dev00 ");
 	}
 
 }
