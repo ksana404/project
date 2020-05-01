bc2c90ba0abd00739a972780ed1686ccb7bc474d
ksana404 <ksana404@163.com>
2020-05-01 16:04:21 +0800

    0501pm commit test01 5月1号下午 提交到本地库 测试01

--- yangyang/src/test/java/com/yg/pj/sys/source/GitTest.java
+++ yangyang/src/test/java/com/yg/pj/sys/source/GitTest.java
@@ -5,6 +5,7 @@ public class GitTest {
 	public static void main(String[] args) {
 		//Git 上传到远程 分支测试
 		//Git 上传到远程 分支测试 02
+		//Git 上传到远程 分支测试 03
 		String str = "";
 		String str01 = "";
 		String str02 = "";
